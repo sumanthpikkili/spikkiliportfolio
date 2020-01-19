cedc2b803b1d78eaea59af6ee54af342dfb61d0c
Sumanth Pikkili <sumanthpikkili@gmail.com>
2020-01-19 17:01:55 +0000

    Updates

---
+++ precache-manifest.1aba75c0480b03b965a7bb7e153ead27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8630b0190ea6d9ef404ad3206e31f27",
+    "url": "/spikkiliportfolio/index.html"
+  },
+  {
+    "revision": "20ba8b9265666b67a5da",
+    "url": "/spikkiliportfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "22d320d53d8606b7889e",
+    "url": "/spikkiliportfolio/static/js/2.8dd36e00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spikkiliportfolio/static/js/2.8dd36e00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20ba8b9265666b67a5da",
+    "url": "/spikkiliportfolio/static/js/main.42e9a4c1.chunk.js"
+  },
+  {
+    "revision": "5eb49f8d65a821d0ecc5",
+    "url": "/spikkiliportfolio/static/js/runtime-main.2f0003e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spikkiliportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
